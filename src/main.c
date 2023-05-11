dbc833b8cbd6cf52e440aaf1fd7ad673454e61b4
CarlosAbarza <127499530+CarlosAbarza@users.noreply.github.com>
2023-05-11 18:53:36 +0000

    avance

--- src/main.c
+++ src/main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "heap.h"
 #include "printGen.h"
 
 int main() {
   Heap *pq = createHeap();
 
-  printMenu();
-  char opcion
+  char opcion[30];
   while (1) {
-    
+    printMenu();
+    scanf("%[^\n]", opcion);
+    getchar();
+    if (strlen(opcion) > 1)
+      strcpy(opcion, "next");
+    switch (opcion[0]) {
+      case '1' :
+        printf("funciona\n");
+        break;
+
+      case '2' :
+        printf("no funciona\n");
+        break;
+
+      case '0' :
+        printf("Hasta luego\n");
+        free(pq);
+        return 0;
+
+      default:
+        printf("Opción inválida. Por favor, ingrese un número válido.\n");
+        break;
+  }
 }
   
 }
